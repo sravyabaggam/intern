eafa6a6c89e62cd14356dafee565d01ec598d48d
sbaggam <sbaggam@open-logix.com>
2016-09-01 20:21:20 +0530

    push

--- sample-registration-form-validation.js
+++ sample-registration-form-validation.js
@@ -8,7 +8,8 @@ var ucountry = document.registration.country;
 var uzip = document.registration.zip;  
 var uemail = document.registration.email;  
 var umsex = document.registration.msex;  
-var ufsex = document.registration.fsex; if(userid_validation(uid,5,12))  
+var ufsex = document.registration.fsex; 
+if(userid_validation(uid,5,12))  
 {  
 if(passid_validation(passid,7,12))  
 {  
